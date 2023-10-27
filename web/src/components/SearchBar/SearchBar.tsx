c4e3b2cd57178f946692903e7101a7c8dbe60af7
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-10-26 20:37:15 -0400

    finish search bar component

--- web/src/components/SearchBar/SearchBar.tsx
+++ web/src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,21 @@
+import { IconSearch } from "@tabler/icons-react"
+import { useEffect, useState } from "react"
+import { useDebounce } from "use-debounce"
+
 const SearchBar = () => {
+  const [search, setSearch] = useState("")
+
   return (
-    <div className=''>
+      <div className="p-2 border border-1 border-neutral focus-within:border-neutral-content rounded-full w-full">
 
+      <form action="/search" method="GET">
+        <div className="flex flex-row gap-3">
+          <button type="submit"><IconSearch></IconSearch></button>
+          <input type="text" placeholder="What do you want to do today?" className="w-full bg-base-100 focus:outline-none"  onChange={(event) => setSearch(event.target.value)} name="query"></input>
+        </div>
+      </form>
 
-    </div>
+      </div>
   )
 }
 
