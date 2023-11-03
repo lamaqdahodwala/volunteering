9ecced5d815ba30b941388ceaa0f5cdf575d2cc2
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-11-03 19:09:31 -0400

    create a better looking card component

--- web/src/components/ScheduleCell/ScheduleCell.tsx
+++ web/src/components/ScheduleCell/ScheduleCell.tsx
@@ -5,6 +5,7 @@ import type {
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 import RecommendedItem from '../RecommendedItem/RecommendedItem'
 import { Link, routes } from '@redwoodjs/router'
+import { DateTime } from 'luxon'
 
 export const QUERY = gql`
   query FindScheduleQuery {
@@ -42,19 +43,24 @@ export const Success = ({
   schedule,
 }: CellSuccessProps<FindScheduleQuery, FindScheduleQueryVariables>) => {
   return (
-    <>
+    <div className="w-full px-32 py-2">
       {schedule.map((val) => {
         let job = val.on_job
         return (
           <div className="card bg-base-300">
-            <h1 className="card-title">{job.datetime}</h1>
-            <div className="card-content">
-              <Link to={routes.jobDetail({id: String( job.id )})} className="link">{job.title}</Link>
+            <div className="card-body">
+              <h1 className="card-title">{DateTime.fromISO( job.datetime ).toLocaleString(DateTime.DATETIME_MED_WITH_WEEKDAY)}</h1>
+              <Link
+                to={routes.jobDetail({ id: String(job.id) })}
+                className="link text-info"
+              >
+                {job.title}
+              </Link>
             </div>
           </div>
         )
       })}
-    </>
+    </div>
   )
   // return (
   // <>{JSON.stringify( schedule )}</>
