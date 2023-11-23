26824957864662f4fe311d4196d7fd0709b53e2c
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-11-22 19:25:14 -0500

    added a button to go to your account page

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -8,14 +8,22 @@ const Navbar = () => {
     <div>
       {auth.isAuthenticated ? (
         <div className="flex flex-row gap-3">
-          <Link to={routes.home()}>Discover</Link>
-          <Link to={routes.schedule()}>Schedule</Link>
-          <Link to={routes.volunteerLog()}>Volunteer Log</Link>
+          <Link className="p-1" to={routes.home()}>Discover</Link>
+          <Link className="p-1" to={routes.schedule()}>Schedule</Link>
+          <Link className="p-1" to={routes.volunteerLog()}>Volunteer Log</Link>
+          <Link className="p-1" to={routes.user({ id: String(auth.currentUser.id) })}>
+
+          <p className='text-info border border-1 p-1 rounded-lg border-neutral'>{auth.currentUser.username}</p>
+          </Link>
         </div>
       ) : (
         <div>
-          <Link className="link" to={routes.login()}>Log in</Link>
-          <Link className="link" to={routes.signup()}>Sign up</Link>
+          <Link className="link" to={routes.login()}>
+            Log in
+          </Link>
+          <Link className="link" to={routes.signup()}>
+            Sign up
+          </Link>
         </div>
       )}
     </div>
