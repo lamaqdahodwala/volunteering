34e3f0776832fcaf7548fabe5087bb3ca145a887
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-11-30 19:15:33 -0500

    create dummy data for tests for jobs

--- api/src/services/jobs/jobs.scenarios.ts
+++ api/src/services/jobs/jobs.scenarios.ts
@@ -1,15 +1,51 @@
 import type { Prisma, Job } from '@prisma/client'
 import type { ScenarioData } from '@redwoodjs/testing/api'
 
-export const standard = defineScenario<Prisma.JobCreateArgs>({
+export const standard = defineScenario<
+  Prisma.JobCreateArgs | Prisma.UserCreateArgs,
+  'job' | 'user'
+>({
+  tag: {
+    one: {
+      data: {
+        id: 1,
+        name: 'Health',
+        description: 'Placeholder',
+      },
+    },
+    two: {
+      data: {
+        id: 2,
+        name: 'Science',
+        description: 'Placeholder',
+      },
+    },
+  },
+  user: {
+    one: {
+      data: {
+        username: 'jfk high school',
+        id: 1,
+        roles: 'business',
+        hashedPassword: 'pass',
+        salt: 'salt',
+      },
+    },
+  },
   job: {
     one: {
       data: {
         title: 'String',
         description: 'String',
         max_signups: 2647526,
+        datetime: new Date(),
         minimum_age: 1236127,
-        manager: { create: { username: 'String4203165' } },
+        manager: { connect: { id: 1 } },
+        tags: {
+          connect: {
+            id: 1,
+          },
+        },
       },
     },
     two: {
@@ -18,7 +54,16 @@ export const standard = defineScenario<Prisma.JobCreateArgs>({
         description: 'String',
         max_signups: 8514952,
         minimum_age: 2965536,
-        manager: { create: { username: 'String2197348' } },
+        datetime: new Date(),
+        manager: { connect: { id: 1 } },
+        tags: {
+          connect: [
+            {
+              id: 1,
+            },
+            { id: 2 },
+          ],
+        },
       },
     },
   },
--- api/src/services/jobs/jobs.test.ts
+++ api/src/services/jobs/jobs.test.ts
@@ -1,6 +1,5 @@
 import type { Job } from '@prisma/client'
 
-import { jobs } from './jobs'
 import type { StandardScenario } from './jobs.scenarios'
 
 // Generated boilerplate tests do not account for all circumstances
@@ -9,10 +8,16 @@ import type { StandardScenario } from './jobs.scenarios'
 //       https://redwoodjs.com/docs/testing#testing-services
 // https://redwoodjs.com/docs/testing#jest-expect-type-considerations
 
-describe('jobs', () => {
-  scenario('returns all jobs', async (scenario: StandardScenario) => {
-    const result = await jobs()
+// describe('jobs', () => {
+//   scenario('returns all jobs', async (scenario: StandardScenario) => {
+//     const result = await jobs()
+//
+//     expect(result.length).toEqual(Object.keys(scenario.job).length)
+//   })
+// })
+//
+describe("search", () => {
+  scenario("searches for a keyword", async() => {
 
-    expect(result.length).toEqual(Object.keys(scenario.job).length)
   })
 })
